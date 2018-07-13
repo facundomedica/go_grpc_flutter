0604a8b27162c013c1667eca6c1666fd7791028e
facundomedica <facundolmedica@gmail.com>
2018-07-12 22:30:39 -0300

    modified server.go

--- server/server.go
+++ server/server.go
@@ -4,6 +4,10 @@ import (
 	"github.com/facundomedica/go_grpc_flutter"
 )
 
-type GoGrpcFlutterServer struct {
-	go_grpc_flutter.GoGrpcFlutterServer
+type AuthServer struct {
+	go_grpc_flutter.AuthServer
+}
+
+type TasksServer struct {
+	go_grpc_flutter.TasksServer
 }
